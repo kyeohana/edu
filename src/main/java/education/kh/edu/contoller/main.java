160dc65183e9dd101b5933727c21dffd40936cde
ykh <ykh231@naver.com>
2024-03-05 16:48:15 +0900

    First Commit

---
+++ src/main/java/education/kh/edu/contoller/main.java
@@ -0,0 +1,15 @@
+package education.kh.edu.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class main {
+    
+    @GetMapping("/main")
+    public String mainUrl() {
+        return "main";
+    }
+    
+}
