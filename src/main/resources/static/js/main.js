686b72444f28348dfdf3a93ccadcb448751d76b3
= <=ykh231@naver.com>
2024-03-15 17:25:35 +0900

    notice commit 2

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,6 @@
 $("document").ready(function () {
     var header = $("header");
     var sections = $("section > ul > li");
-    var headerHovered = false;
 
     $(".header_container").load("/include/header.html");
     $(".footer-container").load("/include/footer.html");
@@ -20,28 +19,13 @@ $("document").ready(function () {
         loopAdditionalSlides: 1,
     });
 
-    $(".banner > .banner2 > li").mouseenter(function () {
-        header.stop().animate({ height: "220px" }, 500);
-    }).mouseleave(function () {
-        setTimeout(function () {
-            if (!headerHovered) {
-                header.stop().animate({ height: "66px" }, 500);
-                $(".banner > .banner2 > li > ul").hide();
-            }
-        }, 10);
-    });
-
     header.mouseenter(function () {
-        headerHovered = true;
         header.stop().animate({ height: "220px" }, 500);
         $(".banner > .banner2 > li > ul").show();
     }).mouseleave(function () {
-        headerHovered = false;
         setTimeout(function () {
-            if (!headerHovered) {
                 header.stop().animate({ height: "66px" }, 500);
                 $(".banner > .banner2 > li > ul").hide();
-            }
         }, 10);
     });
 
--- src/main/resources/static/js/notice.js
+++ src/main/resources/static/js/notice.js
@@ -1,36 +1,4 @@
 $("document").ready(function () {
-    var header = $("header");
-    var sections = $("section > ul > li");
-    var headerHovered = false;
-    var tableBody = $('#body-list tbody')
-
-    $(".header_container").load("/include/header.html");
-    $(".footer-container").load("/include/footer.html");
-
-    $(".banner > .banner2 > li").mouseenter(function () {
-        header.stop().animate({ height: "220px" }, 500);
-    }).mouseleave(function () {
-        setTimeout(function () {
-            if (!headerHovered) {
-                header.stop().animate({ height: "66px" }, 500);
-                $(".banner > .banner2 > li > ul").hide();
-            }
-        }, 10);
-    });
-
-    header.mouseenter(function () {
-        headerHovered = true;
-        header.stop().animate({ height: "220px" }, 500);
-        $(".banner > .banner2 > li > ul").show();
-    }).mouseleave(function () {
-        headerHovered = false;
-        setTimeout(function () {
-            if (!headerHovered) {
-                header.stop().animate({ height: "66px" }, 500);
-                $(".banner > .banner2 > li > ul").hide();
-            }
-        }, 10);
-    });
 
     $.ajax({
         url: '/board/notice/list',
