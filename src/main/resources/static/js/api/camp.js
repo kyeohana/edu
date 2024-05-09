b3f015f85891aea193ec6ff4fb0885e18cd40f53
kyeohana <ykh231@naver.com>
2024-05-09 16:30:26 +0900

    api connection commit

--- src/main/resources/static/js/api/camp.js
+++ src/main/resources/static/js/api/camp.js
@@ -1,9 +1,15 @@
 $("document").ready(function () {
 
-    var mapOptions = {
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
-        zoom: 10
-    };
+    // id가 map인 것을 찾아 mapContainer로 받음
+    let mapContainer = $('#map')[0]  ,
 
-    var map = new naver.maps.Map('map', mapOptions);
+        // 최초 짖도의 중심좌표, 레벨 등을 지정
+        mapOption = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        };
+
+    // 카카오 맵을 띄움
+    let map = new kakao.maps.Map(mapContainer, mapOption);
+
 })
