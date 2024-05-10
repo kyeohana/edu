74908b3410eb5bb42f414e7bd4f06df785c9cbad
kyeohana <ykh231@naver.com>
2024-05-10 17:26:41 +0900

    api connection commit 2

---
+++ src/main/java/education/kh/edu/contoller/CampController.java
@@ -0,0 +1,44 @@
+package education.kh.edu.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CampController {
+
+    @Value("${kakao.map.api.key}")
+    private String kakaoMapKeyApi;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/api/campMap")
+    public String getKakaoMapKeyApi() {
+
+        return kakaoMapKeyApi;
+
+    };
+
+
+    @GetMapping("/api/campSearch")
+    public String getMapSearch(@RequestParam String mapSearch) {
+
+        //https://data.gg.go.kr/portal/data/service/selectServicePage.do?page=2&rows=10&sortColumn=VIEW_CNT&sortDirection=DESC&infId=6243I631A7C7L7M0JR1B21715119&infSeq=3&order=3&loc=
+
+        final String apiKey = "4c754fcc7bce4051b48b3fd84e606fc5";
+
+        String apiUrl = "https://openapi.gg.go.kr/CAMPGRD" +
+                "?Key=" + apiKey +
+                "&Type=json" +
+                "&pIndex=1" +
+                "&pSize+10" +
+                "&SIGUN_NM=" + mapSearch ;
+
+        return restTemplate.getForObject(apiUrl,String.class);
+
+    };
+}
--- src/main/resources/static/js/api/camp.js
+++ src/main/resources/static/js/api/camp.js
@@ -1,15 +1,69 @@
 $("document").ready(function () {
 
-    // id가 map인 것을 찾아 mapContainer로 받음
-    let mapContainer = $('#map')[0]  ,
+    $(".campSearchSubmit").submit(function (event) {
+        event.preventDefault()
 
-        // 최초 짖도의 중심좌표, 레벨 등을 지정
+        var mapSearch = $(".mapSearch").val();
+
+        $.ajax({
+            url : '/api/campSearch',
+            type : 'GET',
+            data : mapSearch,
+            success : function (data) {
+                var data = data
+            }
+
+        })
+
+    })
+
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3
+            center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+            level: 4 // 지도의 확대 레벨
         };
 
-    // 카카오 맵을 띄움
-    let map = new kakao.maps.Map(mapContainer, mapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-})
+    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
+        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
+// 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage // 마커이미지 설정
+    });
+
+// 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    var content = '<div class="customoverlay">' +
+        '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+        '    <span class="title">구의야구공원</span>' +
+        '  </a>' +
+        '</div>';
+
+// 커스텀 오버레이가 표시될 위치입니다
+    var position = new kakao.maps.LatLng(37.54699, 127.09598);
+
+// 커스텀 오버레이를 생성합니다
+    var customOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1
+    });
+});
+
+
+function mapSearch() {
+
+
+
+}
